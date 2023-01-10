33a826cbd5c38999773126ee8a3d6d2e05374f56
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-01-10 18:06:54 +0900

    그리디 알고리즘

---
+++ 프로그래머스/그리디/01.체육복.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @param {*} n : 반 총 인원 수
+ * @param {*} lost : 체육복을 안 가져온 학생(배열로 학생 숫자)
+ * @param {*} reserve : 체육복을 ❗️빌려줄 수 있는❗️ 학생
+ * ==> 내 것만 있는 학생도 있는데, 그 학생은 체육복을 빌려줄 수 없음.
+ */
+let solution = (n, lost, reserve) => {
+    // Algorithm : 왜 그리디일까.. => 최대한 많은 수의 학생들을 체육수업을 듣게 하기 위함..? 
+    for(let value of reserve) {
+        if(value)
+    }
+}
+console.log(5, [2,4], [1, 3, 5]);
